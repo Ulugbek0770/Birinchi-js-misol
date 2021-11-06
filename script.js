4fd24e106ad6c8355487b07785ce3de22b9edfba
Ulugbek <ulugbek>
2021-11-06 22:35:29 +0500

    dars

---
+++ script.js
@@ -0,0 +1,44 @@
+
+var name = prompt("Ismingizni kriting")
+console.log("Mening ismim " + name);
+
+var age = +prompt ("Yoshingizni kriting")
+console.log ("Men " + age + " yoshdaman");
+
+var a = 8
+var b = 4
+var c = 12
+var d = 15
+
+var num = +prompt("8 + 4 = ? ")
+console.log("Misol 1: " + " 8 + 4 = " + (a + b ) + "  Sizning: javob: " + num);
+
+var num = +prompt("8 * 12 = ?")
+console.log("Misol 2: " +  " 8 * 12 = " + ( a * c ) + " Sizning javob: " + num);
+
+
+var num = +prompt( "12 - 4 = ? " )
+console.log("Misol 3: " + " 12 - 4 = " + (c - a) + " Sizning javob: " + num  );
+
+var num = +prompt("8 / 4 = ? ")
+console.log("Misol 4: " + " 8 / 4 = " + ( b / a) + " Sizning javob: " + num);
+
+var num = +prompt(" 15 % 4 = ?")
+console.log("Misol 5: " + " 15 & 4 = " + (d % a) + " Sizning javob: " + num );
+
+var num = + prompt( " 12 * 4 = ? ")
+console.log("Misol 6: " + " 12 * 4 = " + (c * a) + " Sizning javob: " + num );
+
+var num = +prompt ( "12 + 15 = ?")
+console.log("Misol 7: " + " 12 + 15 =" + (d + c) + " Sizning javob: " + num);
+
+var num = +prompt (" 15 % 12 = ?")
+console.log("Misol 8: " + " 15 % 12 = " + (d%c) + " Sizning javob: " + num);
+
+var num = +prompt("15 + 8 = ? ")
+console.log("Misol 9: " + "15 + 8 =" + (d + a) + " Sizning javob: " + num );
+
+var num = +prompt("12 / 4 = ?" )
+console.log("Misol 10: " + "12 / 4 = " + (c / a) + " Sizning javob: " + num );
+
+alert("Consolega kiring")
